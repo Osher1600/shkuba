3e8512d82494d41219fb4fc1a8851d1ec587b590
oshertd160 <oshertd160@gmail.com>
2025-07-30 22:18:18 +0300

    update build and cmake.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
     defaultConfig {
         applicationId = "com.dinari.shkuba"
         minSdk = 29
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
