7f3cb621e3ed5867045e0484c584c1a8d4ff0066
Noga <nogush212@gmail.com>
2025-08-04 11:51:54 +0300

    added first card func for bot

--- app/src/main/cpp/logic/gameBot.h
+++ app/src/main/cpp/logic/gameBot.h
@@ -11,6 +11,7 @@ public:
 	GameBot();
 	void botDropCard(Hand& botHand, Board& board);
 	void playCard(Hand botHand, Board board); //this will add to the playCard func in Hand class.
+	bool botFirstCardChoice(Card firstCard); //returns true if the bot wants to play first in the first mini-round.
 
 private:
 	
